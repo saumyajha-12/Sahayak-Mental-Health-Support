cdc83766747038d0e0259e0baceceba18b64e87c
saumyajha-12 <167021325+saumyajha-12@users.noreply.github.com>
2025-05-31 22:47:04 +0530

    chatbot.py

---
+++ chatbotpy/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+embeddings = download_hugging_face_embeddings()
+
+
+index_name = "chatbotbot"
---
+++ chatbotpy/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Sahayak chatbot",           # Replace with your project name
+    version="0.1.0",                     # Initial version
+    author="Saumya",
+    author_email="spjha1207@gmail.com",
+    packages=find_packages(),            # Automatically find all packages and sub-packages
+    install_requires=[],
+   
+)
